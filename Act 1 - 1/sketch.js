14e0c3f33452851514ec384ae452cc3dd9cb2caa
S-h-a-h-a-b <144931731+S-h-a-h-a-b@users.noreply.github.com>
2024-06-12 13:00:17 +0400

    p5js

---
+++ Act 1 - 1/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200);
+  
+  fill(255,0,0);
+  rect(150,100,100,200);
+  
+  fill(0,0,255);
+  ellipse(200,200,140);
+  
+  fill(0,255,0);
+  triangle(100,250,200,120,300,250);
+  
+}
